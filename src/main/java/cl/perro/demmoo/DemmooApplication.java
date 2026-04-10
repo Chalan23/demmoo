f2ea7fe4770fa86d2790d05ca914395b858d20dc
Chalan23 <ernesto.vilches.chalan@gmail.com>
2026-04-10 03:19:04 -0400

    first commit

---
+++ src/main/java/cl/perro/demmoo/DemmooApplication.java
@@ -0,0 +1,13 @@
+package cl.perro.demmoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemmooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemmooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/perro/demmoo/DemmooApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.perro.demmoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemmooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
